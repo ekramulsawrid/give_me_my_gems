0360abf920bf1a7568f983cdcd21baa90c3c65e7
ekramul000 <51310938+ekramul000@users.noreply.github.com>
2020-02-29 23:14:51 -0500

    added main file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "Board.h"
+#include "State.h"
+#include "Functions.h"
+
+//EKRAMUL SAWRID
+//MANCALA GAME
+
+int main(){
+	Board input;
+	//input.customize(0,1,2,3,4,5,6,7,8,9,10,11,12,13);
+	//input.print();
+	tNode* root=new tNode(input);
+
+	//BFS(root);
+	//DFS(root);
+	//MMS(root);
+	//MultiPlay(input);
+	//Play(input);
+	
+	return 0;
+}
